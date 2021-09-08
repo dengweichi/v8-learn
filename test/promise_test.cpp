4531e2149d3ee0cc18c07bd281379c96b2e3a2a8
dengweichi@cmcm.com <dengweichi@cmcm.com>
2021-09-08 19:16:34 +0800

    feat:增加promise测试用例

--- test/promise_test.cpp
+++ test/promise_test.cpp
@@ -3,106 +3,36 @@
 #include "./base/environment.h"
 #include "libplatform/libplatform.h"
 #include <iostream>
-
+#include <thread>
 static unsigned int global_count = 0;
 
-class AsyncCallBackReadFileTask : public AbstractAsyncTask {
-private:
-    v8::Persistent<v8::Context> persistentContext;
-    v8::Persistent<v8::Function> persistentCallBack;
-    std::string relativePath;
-    v8::Isolate *isolate;
-
-public:
-    AsyncCallBackReadFileTask(v8::Isolate *isolate, v8::Local<v8::Context> context, const char *relativePath, v8::Local<v8::Function> callBack) : AbstractAsyncTask() {
-        this->isolate = isolate;
-        this->persistentContext.Reset(isolate, context);
-        this->persistentCallBack.Reset(isolate, callBack);
-        this->relativePath = std::string(relativePath);
-    }
-    void run() override {
-        v8::Locker locker(this->isolate);
-        v8::Isolate::Scope scope(this->isolate);
-        {
-            v8::HandleScope handleScope(isolate);
-            v8::Local<v8::Context> context = this->persistentContext.Get(this->isolate);
-            v8::Context::Scope context_scope(context);
-            v8::Local<v8::Function> callback = this->persistentCallBack.Get(this->isolate);
-
-            // 获取文件的绝对路径
-            std::string workDir = Environment::GetWorkingDirectory();
-            std::string absolutePath = Environment::NormalizePath(this->relativePath, workDir);
-            if (!Environment::IsAbsolutePath(absolutePath)) {
-                int argc = 1;
-                v8::Local<v8::Value> argv[] = {v8::Exception::Error(v8::String::NewFromUtf8Literal(isolate, "路径错误"))};
-                callback->Call(context, v8::Null(isolate), argc, argv).ToLocalChecked();
-            } else {
-                std::string fileContent = Environment::ReadFile(absolutePath);
-                v8::Local<v8::String> content;
-                EXPECT_TRUE(v8::String::NewFromUtf8(isolate, fileContent.c_str()).ToLocal(&content));
-                int argc = 2;
-                v8::Local<v8::Value> argv[] = {v8::Null(isolate), content};
-                callback->Call(context, v8::Null(isolate), argc, argv).ToLocalChecked();
-            }
-        }
-    }
-    ~AsyncCallBackReadFileTask() {
-        this->persistentContext.Reset();
-        this->persistentCallBack.Reset();
-    }
-};
-
-bool async_callback_read_file_over = false;
 TEST_F(Environment, async_callback_read_file) {
     global_count = 0;
     v8::Isolate *isolate = getIsolate();
-    v8::HandleScope handleScope(isolate);
-    v8::Local<v8::Context> context = v8::Context::New(isolate);
-    v8::Context::Scope context_scope(context);
-    v8::Local<v8::Function> readFile;
-    /**
-     * readFile("path", (error, data) => {
-     *      if(!error) {
-     *          console.log(data);
-     *      }
-     * })
-     */
-    EXPECT_TRUE(v8::Function::New(context, [](const v8::FunctionCallbackInfo<v8::Value> &info) -> void {
-                    v8::Isolate *isolate = info.GetIsolate();
-                    v8::HandleScope handleScope(isolate);
-                    if (info.Length() != 2 || !info[0]->IsString() || !info[1]->IsFunction()) {
-                        isolate->ThrowException(v8::String::NewFromUtf8Literal(isolate, "参数错误"));
-                        return;
-                    }
-                    v8::Local<v8::Context> context = isolate->GetCurrentContext();
-                    v8::Context::Scope context_scope(context);
-                    std::unique_ptr<AsyncCallBackReadFileTask> asyncCallBackReadFileTask;
-                    {
+    {
+        v8::HandleScope handleScope(isolate);
+        v8::Local<v8::Context> context = v8::Context::New(isolate);
+        v8::Context::Scope context_scope(context);
+        v8::Local<v8::Function> function = v8::Function::New(context, [](const v8::FunctionCallbackInfo<v8::Value> &info) -> void {
+            global_count++;
+        }).ToLocalChecked();
 
-                        const char *relativePath = *v8::String::Utf8Value(isolate, info[0]);
-                        std::cout << relativePath << "111111111" << std::endl;
-                        v8::Local<v8::Function> callBack = info[1].As<v8::Function>();
-                        asyncCallBackReadFileTask = std::make_unique<AsyncCallBackReadFileTask>(isolate, context, relativePath, callBack);
-                    }
-                    asyncCallBackReadFileTask->start();
-                    asyncCallBackReadFileTask->join();
-                }).ToLocal(&readFile));
-    EXPECT_TRUE(context->Global()->Set(context, v8::String::NewFromUtf8Literal(isolate, "readFile"), readFile).FromJust());
+        v8::Global<v8::Context> persistentContext(isolate, context);
+        v8::Global<v8::Function> persistentFunction(isolate, function);
 
-    v8::Local<v8::Function> getValue = v8::Function::New(context, [](const v8::FunctionCallbackInfo<v8::Value> &info) -> void {
-                                            async_callback_read_file_over = true;
-                                       }).ToLocalChecked();
-    EXPECT_TRUE(context->Global()->Set(context, v8::String::NewFromUtf8Literal(isolate, "getValue"), getValue).FromJust());
-
-    const char *source = "readFile('./source/test_read_file.json', (error, data) => {\n"
-                         "      if(!error) {\n"
-                         "          getValue(data);\n"
-                         "      }\n"
-                         "      getValue(null);\n"
-                         "});\n";
-    v8::Local<v8::Script> script =
-            v8::Script::Compile(context, v8::String::NewFromUtf8(isolate, source).ToLocalChecked()).ToLocalChecked();
-    script->Run(context).ToLocalChecked();
+        std::thread thread([](v8::Isolate* isolate, v8::Global<v8::Context> persistentContext,
+                              v8::Global<v8::Function> persistentFunction) -> void {
+            v8::Locker locker(isolate);
+            v8::Isolate::Scope scope(isolate);
+            v8::HandleScope handleScope(isolate);
+            v8::Local<v8::Context> context = persistentContext.Get(isolate);
+            v8::Local<v8::Function> callBack = persistentFunction.Get(isolate);
+            callBack->Call(context,v8::Null(isolate), 0 , {}).ToLocalChecked();
+        },isolate, std::move(persistentContext), std::move(persistentFunction));
+        // std::this_thread::sleep_for(std::chrono::seconds(3));
+        thread.join();
+        EXPECT_EQ(global_count, 1);
+    }
 }
 
 
